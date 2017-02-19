ac500f6279f2d7c172ba05f8127dc3ec2b555df1
James Pike <jpike@chilon.net>
2017-02-19 02:01:58 +0000

    server removeTrack api takes position and removes track with id closest to it

--- server/music-queue.ts
+++ server/music-queue.ts
@@ -4,7 +4,7 @@ import { SocketCommunicator } from './socket-communicator'
 import { Track } from '../src/app/track'
 
 // TODO: read from fs
-let musicQueue: Track[] = []
+const musicQueue: Track[] = []
 
 const saveQueue = _.debounce(() => {
   console.log('TODO: save queue')
@@ -16,9 +16,31 @@ export function insertTrack(socket: SocketCommunicator, { position, track }: { p
   socket.broadcast({ insert: position, track })
 }
 
-export function removeTrack(socket: SocketCommunicator, { trackId }, { trackId: string }): void {
-  musicQueue = musicQueue.filter(track => track.id !== trackId)
-  socket.broadcast({ remove: trackId })
+export function removeTrack(
+  socket: SocketCommunicator,
+  { position, trackId } : { position: number, trackId: string }
+): void
+{
+  // find the entry for trackId closest to `position`
+  let idx = -1
+  let nextIdx
+  let bestDistance = Number.MAX_VALUE
+  while ((nextIdx =  musicQueue.findIndex(track => track.id !== trackId, idx + 1)) !== -1) {
+    const distance = Math.abs(nextIdx - position)
+    if (distance > bestDistance) {
+      // we're getting further so give up
+      break
+    }
+    else {
+      bestDistance = distance
+      idx = nextIdx
+    }
+  }
+
+  if (idx !== -1) {
+    musicQueue.splice(idx, 1)
+    socket.broadcast({ remove: idx })
+  }
 }
 
 export function getMusicQueue(socket: SocketCommunicator): void {
