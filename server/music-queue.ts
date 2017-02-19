e78a347b3f669a1bf2d51335d77dd26210c8dd42
James Pike <jpike@chilon.net>
2017-02-19 14:03:04 +0000

    save/load queue to/from fs

--- server/music-queue.ts
+++ server/music-queue.ts
@@ -1,14 +1,40 @@
 import * as _ from 'lodash'
+import * as promisify from 'es6-promisify'
+import * as fs from 'fs'
 
 import { SocketCommunicator } from './socket-communicator'
 import { Track } from '../src/app/track'
 
-// TODO: read from fs
+const mkpath = promisify(require('mkpath'))
+const writeFile = promisify(fs.writeFile)
+const readFile = promisify(fs.readFile)
+
 const musicQueue: Track[] = []
 
-const saveQueue = _.debounce(() => {
-  console.log('TODO: save queue')
+const getConfigPath = () => `${process.env.HOME}/.config/share-deezer`
+
+const saveQueue = _.debounce(async () => {
+  console.log('saving queue')
+  try {
+    const configPath = getConfigPath()
+    await mkpath(configPath)
+    await writeFile(`${configPath}/default.playlist.json`, JSON.stringify(musicQueue, null, 2))
+  }
+  catch (e) {
+    console.log('error saving queue', e.stack || e)
+  }
 }, 1000)
+
+const loadQueue = _.once(async () => {
+  try {
+    const buffer = await readFile(`${getConfigPath()}/default.playlist.json`)
+    musicQueue.splice(0, musicQueue.length, ...JSON.parse(buffer.toString()))
+  }
+  catch (e) {
+    console.log('error loading queue', e.stack || e)
+    musicQueue.splice(0)
+  }
+})
 
 export function insertTrack(socket: SocketCommunicator, { position, track }: { position: number, track: Track }): void {
   musicQueue.splice(position, 0, track)
@@ -24,8 +50,8 @@ export function removeTrack(
   // find the entry for trackId closest to `position`
   let idx = -1
   let nextIdx
-  let bestDistance = Number.MAX_VALUE
-  while ((nextIdx =  musicQueue.findIndex(track => track.id !== trackId, idx + 1)) !== -1) {
+  let bestDistance = Infinity
+  while ((nextIdx = musicQueue.findIndex(track => track.id !== trackId, idx + 1)) !== -1) {
     const distance = Math.abs(nextIdx - position)
     if (distance > bestDistance) {
       // we're getting further so give up
@@ -43,6 +69,7 @@ export function removeTrack(
   }
 }
 
-export function getMusicQueue(socket: SocketCommunicator): void {
+export async function getMusicQueue(socket: SocketCommunicator): Promise<void> {
+  await loadQueue()
   socket.send({ musicQueue })
 }
