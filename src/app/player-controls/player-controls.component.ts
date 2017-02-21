d287b04067b17663319c136e7f406f145380c6b1
James Pike <jpike@chilon.net>
2017-02-21 20:13:54 +0000

    player controls pause button

--- src/app/player-controls/player-controls.component.ts
+++ src/app/player-controls/player-controls.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core'
+
 import { DeezerPlayer } from '../deezer-player/deezer-player.service'
+import { CurrentTrack } from '../music-queue/current-track.service'
 
 @Component({
   selector: 'player-controls',
@@ -7,7 +9,7 @@ import { DeezerPlayer } from '../deezer-player/deezer-player.service'
   styleUrls: ['./player-controls.component.scss']
 })
 export class PlayerControlsComponent {
-  constructor(private deezerPlayer: DeezerPlayer) {
+  constructor(private deezerPlayer: DeezerPlayer, private currentTrack: CurrentTrack) {
     // TODO: read mute status from local storage and apply to deezerPlayer
     const muted = window.localStorage.getItem('muted')
     if (! muted)
@@ -24,5 +26,11 @@ export class PlayerControlsComponent {
       deezerPlayer.activate()
       window.localStorage.removeItem('muted')
     }
+  }
+
+  private play() {
+  }
+
+  private pause() {
   }
 }
