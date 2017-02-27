15898a8c9e39f05f742c440dbcd70a199613bdc6
James Pike <jpike@chilon.net>
2017-02-27 22:57:43 +0000

    current track component ondestroy

--- src/app/current-track/current-track.component.ts
+++ src/app/current-track/current-track.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core'
 import { Observable } from 'rxjs/Observable'
 import 'rxjs/add/observable/combineLatest'
 
+import { OnDestroy } from '../on-destroy'
 import { CurrentTrack, CurrentTrackStatus } from './current-track.service'
 import { MusicQueue } from '../music-queue/music-queue.service'
 import { Track } from '../track'
@@ -11,18 +12,19 @@ import { Track } from '../track'
   templateUrl: './current-track.component.html',
   styleUrls: ['./current-track.component.scss']
 })
-export class CurrentTrackComponent {
+export class CurrentTrackComponent extends OnDestroy {
   private trackStream: Observable<CurrentTrackStatus>
   private track: Track | null
   // elapsed to nearest second
   private elapsed = 0
 
   constructor(currentTrack: CurrentTrack, private musicQueue: MusicQueue) {
+    super()
     this.trackStream = currentTrack.status
   }
 
   ngOnInit() {
-    return Observable.combineLatest(
+    const trackInfo = Observable.combineLatest(
       this.trackStream,
       this.musicQueue.tracks,
       (trackStatus: CurrentTrackStatus, tracks) => ({trackStatus, tracks})
@@ -30,6 +32,10 @@ export class CurrentTrackComponent {
     .subscribe(({ trackStatus, tracks }) => {
       this.track = tracks[trackStatus.trackIdx]
       this.elapsed = Math.round(trackStatus.elapsed / 1000) * 1000
+    })
+
+    this.onDestroy(() => {
+      trackInfo.unsubscribe()
     })
   }
 }
