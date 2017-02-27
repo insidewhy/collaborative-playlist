77a75967d2ff5a544ff5840b6345150c60fb11e5
James Pike <jpike@chilon.net>
2017-02-27 23:54:25 +0000

    angular 2.4.8 -> 4.0.0-rc.1, use the new goods

--- src/app/current-track/current-track.component.ts
+++ src/app/current-track/current-track.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core'
 import { Observable } from 'rxjs/Observable'
 import 'rxjs/add/observable/combineLatest'
 
-import { OnDestroy } from '../on-destroy'
 import { CurrentTrack, CurrentTrackStatus } from './current-track.service'
 import { MusicQueue } from '../music-queue/music-queue.service'
 import { Track } from '../track'
@@ -12,31 +11,18 @@ import { Track } from '../track'
   templateUrl: './current-track.component.html',
   styleUrls: ['./current-track.component.scss']
 })
-export class CurrentTrackComponent extends OnDestroy {
-  // TODO: replace all this with a single stream when
-  //       https://github.com/angular/angular/pull/13297 lands
+export class CurrentTrackComponent {
   private trackInfo: Observable<any>
-  private track: Track | null
-  // elapsed to nearest second
-  private elapsed = 0
 
   constructor(currentTrack: CurrentTrack, private musicQueue: MusicQueue) {
-    super()
     this.trackInfo = Observable.combineLatest(
       currentTrack.status,
       this.musicQueue.tracks,
       (trackStatus: CurrentTrackStatus, tracks) => ({trackStatus, tracks})
     )
-  }
-
-  ngOnInit() {
-    const trackInfo = this.trackInfo.subscribe(({ trackStatus, tracks }) => {
-      this.track = tracks[trackStatus.trackIdx]
-      this.elapsed = Math.round(trackStatus.elapsed / 1000) * 1000
-    })
-
-    this.onDestroy(() => {
-      trackInfo.unsubscribe()
-    })
+    .map(({ trackStatus, tracks }) => ({
+      track: tracks[trackStatus.trackIdx],
+      elapsed: Math.round(trackStatus.elapsed / 1000) * 1000,
+    }))
   }
 }
