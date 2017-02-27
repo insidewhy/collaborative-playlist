f5bb1d58aad041ea0bcb95273330f1450efba64f
James Pike <jpike@chilon.net>
2017-02-27 23:18:32 +0000

    simplify current track component a little

--- src/app/current-track/current-track.component.ts
+++ src/app/current-track/current-track.component.ts
@@ -13,23 +13,24 @@ import { Track } from '../track'
   styleUrls: ['./current-track.component.scss']
 })
 export class CurrentTrackComponent extends OnDestroy {
-  private trackStream: Observable<CurrentTrackStatus>
+  // TODO: replace all this with a single stream when
+  //       https://github.com/angular/angular/pull/13297 lands
+  private trackInfo: Observable<any>
   private track: Track | null
   // elapsed to nearest second
   private elapsed = 0
 
   constructor(currentTrack: CurrentTrack, private musicQueue: MusicQueue) {
     super()
-    this.trackStream = currentTrack.status
-  }
-
-  ngOnInit() {
-    const trackInfo = Observable.combineLatest(
-      this.trackStream,
+    this.trackInfo = Observable.combineLatest(
+      currentTrack.status,
       this.musicQueue.tracks,
       (trackStatus: CurrentTrackStatus, tracks) => ({trackStatus, tracks})
     )
-    .subscribe(({ trackStatus, tracks }) => {
+  }
+
+  ngOnInit() {
+    const trackInfo = this.trackInfo.subscribe(({ trackStatus, tracks }) => {
       this.track = tracks[trackStatus.trackIdx]
       this.elapsed = Math.round(trackStatus.elapsed / 1000) * 1000
     })
