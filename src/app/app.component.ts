df178c2c20d20c3839953dd15277d500c407df7f
James Pike <jpike@chilon.net>
2017-05-06 19:31:23 +0100

    scroll to top when navigating to search results

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core'
+import { Router, NavigationStart } from '@angular/router'
+import 'rxjs/add/operator/pairwise'
+const defer = require('lodash/defer')
+
+const searchUrlRegex = /^\/search\//
 
 @Component({
   selector: 'app-root',
@@ -6,4 +11,21 @@ import { Component } from '@angular/core'
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(router: Router) {
+    let scrollBackup = 0
+
+    router.events
+    .filter(event => event instanceof NavigationStart)
+    .map((event: NavigationStart) => event.url)
+    .pairwise()
+    .subscribe(([prevUrl, url]) => {
+      if (prevUrl === '/' && url.match(searchUrlRegex)) {
+        scrollBackup = window.scrollY
+        window.scrollTo(0, 0)
+      }
+      else if (url === '/' && prevUrl.match(searchUrlRegex)) {
+        defer(() => { window.scrollTo(0, scrollBackup) })
+      }
+    })
+  }
 }
