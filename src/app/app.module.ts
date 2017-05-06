8d7dbfdad2fc1c2c075679f4bef0b992ee6acf68
James Pike <jpike@chilon.net>
2017-05-06 13:08:52 +0100

    latest versions of all deps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 import { FormsModule } from '@angular/forms'
 import { JsonpModule } from '@angular/http'
-import { MdIconModule, MdIconRegistry } from '@angular2-material/icon'
+import { MdIconModule, MdIconRegistry } from '@angular/material'
 
 import { AppComponent } from './app.component'
 import { ServerSocket } from './server-socket.service'
