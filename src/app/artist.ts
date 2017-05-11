e1f7aa6b716015f5ab948635abd73999df44d24b
James Pike <jpike@chilon.net>
2017-05-11 23:41:50 +0100

    remove commas from interfaces

--- src/app/artist.ts
+++ src/app/artist.ts
@@ -1,3 +1,3 @@
 export interface Artist {
-  name: String,
+  name: String
 }
--- src/app/track.ts
+++ src/app/track.ts
@@ -2,9 +2,9 @@ import { Artist } from './artist'
 import { Album } from './album'
 
 export interface Track {
-  id: string,
-  title: string,
-  album: Album,
-  artist: Artist,
-  duration: number,
+  id: string
+  title: string
+  album: Album
+  artist: Artist
+  duration: number
 }
