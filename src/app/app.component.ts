6455f406253be8b20a4211abf9e3760432bdae6e
James Pike <jpike@chilon.net>
2017-05-25 16:50:28 +0100

    use delay to ensure scroll position reset when returning to queue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core'
 import { Router, NavigationStart } from '@angular/router'
 import 'rxjs/add/operator/pairwise'
-const defer = require('lodash/defer')
 
 const searchUrlRegex = /^\/search\//
 
@@ -20,11 +19,13 @@ export class AppComponent {
     .pairwise()
     .subscribe(([prevUrl, url]) => {
       if (url.match(searchUrlRegex) && url !== prevUrl) {
-        scrollBackup = window.scrollY
+        if (prevUrl === '/')
+          scrollBackup = window.scrollY
         window.scrollTo(0, 0)
       }
       else if (url === '/' && prevUrl.match(searchUrlRegex)) {
-        defer(() => { window.scrollTo(0, scrollBackup) })
+        window.scrollTo(0, scrollBackup)
+        setTimeout(() => { window.scrollTo(0, scrollBackup) }, 50)
       }
     })
   }
