bb6a41076122e6cb0770339965c3324e9e5b69e5
James Pike <jpike@chilon.net>
2017-09-08 01:29:41 +0100

    suspend some redraws on elements scrolled out of view, big speed up

--- src/app/music-queue/queued-track.component.ts
+++ src/app/music-queue/queued-track.component.ts
@@ -1,6 +1,11 @@
-import { Component, ChangeDetectionStrategy, Input } from '@angular/core'
+import { AfterViewInit, Component, ChangeDetectionStrategy, ElementRef, Input } from '@angular/core'
+import { Observable } from 'rxjs/Observable'
+import { BehaviorSubject } from 'rxjs/BehaviorSubject'
 import 'rxjs/add/operator/distinctUntilChanged'
+import 'rxjs/add/operator/do'
+import 'rxjs/add/observable/combineLatest'
 
+import scrolledIntoView from './scrolled-into-view'
 import { CurrentTrack } from '../current-track/current-track.service'
 import { SelectedTracks } from './selected-tracks.service'
 import { MusicQueue } from './music-queue.service'
@@ -12,21 +17,34 @@ import { Track } from '../track'
   styleUrls: ['./queued-track.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class QueuedTrackComponent {
+export class QueuedTrackComponent implements AfterViewInit {
   @Input()
   private index: number
   @Input()
   private track: Track
 
+  private scrollObservables: BehaviorSubject< Observable<boolean> > = new BehaviorSubject(Observable.from([false]))
+
+  public hasSelection: Observable<boolean> = Observable.combineLatest(
+    this.selectedTracks.indexes.map(indexes => indexes.size > 0).distinctUntilChanged(),
+    this.scrollObservables.switchMap(val => val),
+    (hasSelection, inView) => hasSelection && inView
+  )
+
   public isSelected = this.selectedTracks.indexes.map(indexes => indexes.has(this.index)).distinctUntilChanged()
-  public hasSelection = this.selectedTracks.indexes.map(indexes => indexes.size > 0).distinctUntilChanged()
   public isPlaying = this.currentTrack.index.map(index => index === this.index).distinctUntilChanged()
 
   constructor(
     private currentTrack: CurrentTrack,
     private selectedTracks: SelectedTracks,
-    private musicQueue: MusicQueue
-  ) {}
+    private musicQueue: MusicQueue,
+    private elementRef: ElementRef,
+  ) { }
+
+  ngAfterViewInit() {
+    const element = this.elementRef.nativeElement as Element
+    this.scrollObservables.next(scrolledIntoView(element, document.body, window))
+  }
 
   toggle(selectRange: boolean) {
     this.selectedTracks.toggle(this.index, selectRange)
---
+++ src/app/music-queue/scrolled-into-view.ts
@@ -0,0 +1,40 @@
+import { Observable } from 'rxjs/Observable'
+import 'rxjs/add/observable/fromEvent'
+import 'rxjs/add/operator/shareReplay'
+import 'rxjs/add/operator/throttleTime'
+
+type ScrollObservable = Observable<any>
+const scrollEventObservables: WeakMap<Element | Window, ScrollObservable> = new WeakMap()
+
+export default function(
+  element: Element,
+  parentElement: Element = null,
+  scrollTarget: Element | Window = null,
+): ScrollObservable {
+  if (! parentElement) {
+    const { parentNode } = element
+    if (! parentNode) {
+      throw new Error('element has no parent')
+    }
+    parentElement = parentNode as Element
+  }
+
+  if (! scrollTarget)
+    scrollTarget = parentElement
+
+  let parentScrolls = scrollEventObservables.get(scrollTarget)
+  if (! parentScrolls) {
+    parentScrolls = Observable.fromEvent(scrollTarget, 'scroll')
+      .throttleTime(1000 / 60)
+      .shareReplay(1)
+    scrollEventObservables.set(parentElement, parentScrolls)
+  }
+
+  return parentScrolls.map((scrollEvent: any) => {
+    // TODO: calculate from parentElement
+    const scrollTop = 0
+    const scrollBottom = scrollTop + parentElement.clientHeight
+    const { top, bottom } = element.getBoundingClientRect()
+    return top < scrollBottom && bottom > scrollTop
+  })
+}
