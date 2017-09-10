ccec23da03a9f4ef92de555b7e341e5e58cdf965
James Pike <jpike@chilon.net>
2017-09-10 20:16:16 +0100

    observable inputs with base class + ngOnChanges hook

--- src/app/music-queue/queued-track.component.ts
+++ src/app/music-queue/queued-track.component.ts
@@ -1,11 +1,32 @@
-import { Component, ChangeDetectionStrategy, Input } from '@angular/core'
-import { BehaviorSubject } from 'rxjs/BehaviorSubject'
+import { Component, ChangeDetectionStrategy, Input, OnChanges, SimpleChange } from '@angular/core'
+import { ReplaySubject } from 'rxjs/ReplaySubject'
+import { Observable } from 'rxjs/Observable'
 import 'rxjs/add/operator/distinctUntilChanged'
+import 'rxjs/add/operator/buffer'
 
 import { CurrentTrack } from '../current-track/current-track.service'
 import { SelectedTracks } from './selected-tracks.service'
 import { MusicQueue } from './music-queue.service'
 import { Track } from '../track'
+
+class ObservableInputs implements OnChanges {
+  private __observedInputs: Map<string, ReplaySubject<any>> = new Map()
+
+  public ngOnChanges(changes: { [key: string]: SimpleChange }) {
+    console.log('changes')
+    for (let key in changes) {
+      const subject = this.__observedInputs.get(key)
+      if (subject)
+        subject.next(changes[key].currentValue)
+    }
+  }
+
+  protected observeInput<T>(propName: string): Observable<T> {
+    const subject = new ReplaySubject<T>(1)
+    this.__observedInputs.set(propName, subject)
+    return subject.asObservable()
+  }
+}
 
 @Component({
   selector: 'app-queued-track',
@@ -13,21 +34,19 @@ import { Track } from '../track'
   styleUrls: ['./queued-track.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class QueuedTrackComponent {
-  // https://github.com/angular/angular/issues/5689
-  public indexStream = new BehaviorSubject<number>(-1)
-  @Input()
-  private set index(value: number) { this.indexStream.next(value) }
+export class QueuedTrackComponent extends ObservableInputs {
+  @Input() public index: number
+  public index$ = this.observeInput<number>('index')
 
   @Input()
-  private track: Track
+  public track: Track
 
-  public isSelected = this.indexStream.switchMap(
+  public isSelected = this.index$.switchMap(
     index => this.selectedTracks.indexes.map(indexes => indexes.has(index)).distinctUntilChanged()
   )
 
   public hasSelection = this.selectedTracks.indexes.map(indexes => indexes.size > 0).distinctUntilChanged()
-  public isPlaying = this.indexStream.switchMap(
+  public isPlaying = this.index$.switchMap(
     index => this.currentTrack.index.map(playingIndex => index === playingIndex).distinctUntilChanged()
   )
 
@@ -35,9 +54,11 @@ export class QueuedTrackComponent {
     private currentTrack: CurrentTrack,
     private selectedTracks: SelectedTracks,
     private musicQueue: MusicQueue
-  ) {}
+  ) {
+    super()
+  }
 
   toggle(selectRange: boolean) {
-    this.selectedTracks.toggle(this.indexStream.value, selectRange)
+    this.selectedTracks.toggle(this.index, selectRange)
   }
 }
