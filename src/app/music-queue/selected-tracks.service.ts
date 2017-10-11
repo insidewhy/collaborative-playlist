69dfb35934c4f63a35b45d617cccdd96f6abaef5
James Pike <jpike@chilon.net>
2017-10-11 20:36:03 +0100

    wip 5

--- src/app/music-queue/selected-tracks.service.ts
+++ src/app/music-queue/selected-tracks.service.ts
@@ -6,7 +6,7 @@ const range = require('lodash/range')
 const sortBy = require('lodash/sortBy')
 
 import { Source } from '../source'
-import { MusicQueue, TrackWithIndex } from './music-queue.service'
+import { MusicQueue, TrackWithIndex, Change } from './music-queue.service'
 
 const mapSet = (set, callback) => {
   const ret = []
@@ -21,6 +21,10 @@ interface Toggle {
   selectRange: boolean
 }
 
+function isToggle(operator: Toggle | Change): operator is Toggle {
+  return operator.hasOwnProperty('index')
+}
+
 @Injectable()
 export class SelectedTracks extends Source {
   private toggle$ = new ReplaySubject<Toggle>(1)
@@ -28,7 +32,6 @@ export class SelectedTracks extends Source {
   private delete$ = new ReplaySubject<null>(1)
   private move$ = new ReplaySubject<number>(1)
 
-  // mega TODO: should be reactive
   rangeStartIdx = this.toggle$
     .filter(({ selectRange }) => ! selectRange)
     .map(({ index }) => index)
@@ -46,24 +49,6 @@ export class SelectedTracks extends Source {
 
   constructor(private musicQueue: MusicQueue) {
     super()
-
-    // mega TODO: move into this.getIndexes()
-    this.reactTo(
-      musicQueue.changes.withLatestFrom(this.indexes),
-      ([ change, indexes ]) => {
-        const { moveFrom } = change
-        if (moveFrom !== undefined) {
-          const { to } = change
-          if (indexes.has(moveFrom)) {
-            const newIndexes = new Set<number>(indexes)
-            newIndexes.delete(moveFrom)
-            newIndexes.add(to)
-            // mega TODO: move higher
-            // this.indexes.next(newIndexes)
-          }
-        }
-      },
-    )
 
     this.reactTo(
       this.delete$.withLatestFrom(this.selectedTracks),
@@ -84,31 +69,40 @@ export class SelectedTracks extends Source {
   }
 
   private getIndexes() {
-    return Observable.merge(this.toggle$, this.clear$)
+    return Observable.merge(this.toggle$, this.clear$, this.musicQueue.changes)
     .withLatestFrom(this.rangeStartIdx)
     .scan(
-      ({ indexes, prevIndexes }, [ toggle, rangeStartIdx ]) => {
-        // a null toggle means clear
-        if (! toggle)
+      ({ indexes, prevIndexes }, [ operation, rangeStartIdx ]) => {
+        // clear
+        if (! operation)
           return { indexes: new Set<number>(), prevIndexes: indexes }
 
-        const { index, selectRange } = toggle
-
-        if (selectRange && rangeStartIdx !== -1) {
-          const newIndexes = new Set(prevIndexes)
-          range(rangeStartIdx, index + (index > rangeStartIdx ? 1 : -1)).forEach(indexToSelect => {
-            if (! newIndexes.has(indexToSelect)) {
-              newIndexes.add(indexToSelect)
-            }
-          })
-          return { indexes: newIndexes, prevIndexes: indexes }
+        if (isToggle(operation)) {
+          const { index, selectRange } = operation
+          if (selectRange && rangeStartIdx !== -1) {
+            const newIndexes = new Set(prevIndexes)
+            range(rangeStartIdx, index + (index > rangeStartIdx ? 1 : -1)).forEach(indexToSelect => {
+              if (! newIndexes.has(indexToSelect)) {
+                newIndexes.add(indexToSelect)
+              }
+            })
+            return { indexes: newIndexes, prevIndexes: indexes }
+          } else {
+            const newIndexes = new Set(indexes)
+            if (newIndexes.has(index))
+              newIndexes.delete(index)
+            else
+              newIndexes.add(index)
+            return { indexes: newIndexes, prevIndexes: indexes }
+          }
         } else {
-          const newIndexes = new Set(indexes)
-          if (newIndexes.has(index))
-            newIndexes.delete(index)
-          else
-            newIndexes.add(index)
-          return { indexes: newIndexes, prevIndexes: indexes }
+          const { moveFrom, to } = operation
+          if (indexes.has(moveFrom)) {
+            const newIndexes = new Set<number>(indexes)
+            newIndexes.delete(moveFrom)
+            newIndexes.add(to)
+            return { indexes: newIndexes, prevIndexes: indexes }
+          }
         }
       },
       { indexes: new Set<number>(), prevIndexes: new Set<number>() }
